584b2ea74b1555b7d7d966ff19befaadc420b6bb
Andrii Ivanchenko <Andriy.ivanchenko91@gmail.com>
2023-05-09 13:57:59 +0200

    Frontend changes, minor bug-fixes, design imps

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 const reader = new FileReader();
 if (window.FileList && window.File && window.FileReader) {
-const fileUploadButton = document.querySelector(".fileSelector");
+const fileUploadButton = document.querySelector(".file-selector");
 fileUploadButton.addEventListener("change",(event)=>{
     file = event.target.files[0];
     reader.readAsText(file);
@@ -9,42 +9,42 @@ fileUploadButton.addEventListener("change",(event)=>{
 }
     function readContent (){
         reader.addEventListener('load',()=>{
-            const result = reader.result;
-            
+            const readFile = reader.result;
+            const parsedJson = JSON.parse(readFile)
+            console.log (deepCheck(parsedJson))
         })
         }
-        function deepCheck(obj, results) {
+        function deepCheck(parsedJson, results) {
             // Initialize the results array if it is not passed as a parameter
             results = results || [];
           
             // Check if the object is null or undefined
-            if (obj == null || typeof obj !== 'object') {
+            if (parsedJson == null || typeof parsedJson !== 'object') {
               return results;
             }
           
             // Check if the object is an array
-            if (Array.isArray(obj)) {
+            if (Array.isArray(parsedJson)) {
               // If the object is an array, recursively check all its elements
-              for (let i = 0; i < obj.length; i++) {
-                deepCheck(obj[i], results);
+              for (let i = 0; i < parsedJson.length; i++) {
+                deepCheck(parsedJson[i], results);
               }
             } else {
               // If the object is not an array, recursively check all the keys and values of the object
-              for (let key in obj) {
-                if (obj.hasOwnProperty(key)) {
-                  deepCheck(obj[key], results);
+              for (let key in parsedJson) {
+                if (parsedJson.hasOwnProperty(key)) {
+                  deepCheck(parsedJson[key], results);
                   // Check if the key is "EvseStatus" and its value is not "Available" or "Occupied"
-                  if (key === 'EvseStatus' && obj[key] !== 'Available' && obj[key] !== 'Occupied') {
+                  if (key === 'EvseStatus' && parsedJson[key] !== 'Available' && parsedJson[key] !== 'Occupied') {
                     // If the value is not "Available" or "Occupied", add an object with "EvseID" and "EvseStatus" to the results array
                     results.push({
-                      EvseID: obj.EvseID,
-                      EvseStatus: obj.EvseStatus
+                      EvseID: parsedJson.EvseID,
+                      EvseStatus: parsedJson.EvseStatus
                     });
                   }
                 }
               }
             }
-          
-            // Return the results array
+            
             return results;
           }

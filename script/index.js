1493de2ae994bc37f39d8c0a14ed3819f890eaf7
Andriy Ivanchenko <Andriy.ivanchenko91@gmail.com>
2023-05-11 15:32:46 +0200

    Final fixes are applied.

--- script/index.js
+++ script/index.js
@@ -1,90 +1,135 @@
 const reader = new FileReader();
-const mainContentSection = document.querySelector(".main-content-container")
+const statusText = document.querySelector(".status-text");
+const mainContentSection = document.querySelector(".main-content-container");
+
 if (window.FileList && window.File && window.FileReader) {
-const fileUploadButton = document.querySelector(".file-selector");
-fileUploadButton.addEventListener("change",(event)=>{
-    file = event.target.files[0];
+  const fileUploadButton = document.querySelector(".file-selector");
+
+  fileUploadButton.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    if (file.type !== "application/json") {
+      const errorMessage = "Error: Invalid file type. Please upload a JSON file.";
+      console.log(errorMessage);
+      showError(errorMessage);
+      return;
+    }
+
+    // Validate if the file is not empty
+    if (file.size === 0) {
+      const errorMessage = "Error: Empty file. Please upload a non-empty JSON file.";
+      console.log(errorMessage);
+      showError(errorMessage);
+      return;
+    }
+
+    statusText.classList.add("status-done");
+    statusText.classList.remove("waiting-for-input");
+    statusText.innerText = "Upload is done";
+
     reader.readAsText(file);
     readContent();
-});
+  });
 }
-    function readContent (){
-        reader.addEventListener('load',()=>{
-          mainContentSection.querySelector(".file-upload-button").classList.add("inactive");
-          document.querySelector(".loading-spinner").classList.remove("inactive");
-          setTimeout(() => {
-            document.querySelector(".loading-spinner").classList.add("inactive");
-          }, 3000);
 
-            const readFile = reader.result;
-            const parsedJson = JSON.parse(readFile) //converting string into JS object
-        const resultArray = (deepCheck(parsedJson)) //function call to process the JSON
-        let resultString ="NUMBER;EVSEID;EVSESTATUS\n";//completing the CSV format
-        let counter=0;
-        resultArray.forEach(obj => {
-          counter++;
-          let entries = Object.entries(obj)
-let data = entries.map( ([key, val] = entry) => {
-    if(key==="EvseID"){
-    resultString = resultString + `${counter};${val};`
-  }
-  else{
-    resultString = resultString + `${val}\n`;}
-});
+function showError(errorMessage) {
+  statusText.innerText = errorMessage;
+  statusText.classList.add("status-error");
+  statusText.classList.remove("waiting-for-input");
+}
 
-        });
-        console.log (resultString)
-        console.log (deepCheck(parsedJson))
-        function download(data, filename, type) {
-          var file = new Blob([data], {type: type});
-          if (window.navigator.msSaveOrOpenBlob) // IE10+
-              window.navigator.msSaveOrOpenBlob(file, filename);
-          else { // Others
-              var a = document.createElement("a"),
-                      url = URL.createObjectURL(file);
-              a.href = url;
-              a.download = filename;
-              document.body.appendChild(a);
-              a.click();
-              setTimeout(function() {
-                  document.body.removeChild(a);
-                  window.URL.revokeObjectURL(url);  
-              }, 0); 
-          }
-      }
-        })
+function readContent() {
+  reader.addEventListener("load", () => {
+    const readFile = reader.result;
+    let parsedJson;
+    try {
+      parsedJson = JSON.parse(readFile); // Convert string into JS object
+    } catch (error) {
+      const errorMessage = "Error: Invalid JSON format. Please upload a file in the specified format.";
+      console.log(errorMessage);
+      showError(errorMessage);
+      return;
+    }
+
+    // Validate if the uploaded JSON has the expected Hubject structure
+    if (!parsedJson || !parsedJson.EvseStatuses || !parsedJson.EvseStatuses.OperatorEvseStatus) {
+      const errorMessage = "Error: Invalid JSON format. Please upload a file in the specified format.";
+      console.log(errorMessage);
+      showError(errorMessage);
+      return;
+    }
+
+    mainContentSection.querySelector(".file-upload-button").classList.add("inactive");
+    statusText.innerText = "Processing...";
+    document.querySelector(".loading-spinner").classList.remove("inactive");
+
+    setTimeout(() => {
+      document.querySelector(".loading-spinner").classList.add("inactive");
+      document.querySelector(".download-button").classList.remove("inactive");
+      statusText.innerText = "Done, file is ready for download";
+    }, 3000);
+
+    const resultArray = deepCheck(parsedJson); // Function call to process the JSON
+    let resultString = "NUMBER;EVSEID;EVSESTATUS\n"; // Completing the CSV format
+    let counter = 0;
+
+    resultArray.forEach((obj) => {
+      counter++;
+      let entries = Object.entries(obj);
+      entries.forEach(([key, val]) => {
+        if (key === "EvseID") {
+          resultString += `${counter};${val};`;
+        } else {
+          resultString += `${val}\n`;
         }
-        function deepCheck(parsedJson, results) {
-            // Initialize the results array if it is not passed as a parameter
-            results = results || [];
-          
-            // Check if the object is null or undefined
-            if (parsedJson == null || typeof parsedJson !== 'object') {
-              return results;
-            }
-          
-            // Check if the object is an array
-            if (Array.isArray(parsedJson)) {
-              // If the object is an array, recursively check all its elements
-              for (let i = 0; i < parsedJson.length; i++) {
-                deepCheck(parsedJson[i], results);
-              }
-            } else {
-              // If the object is not an array, recursively check all the keys and values of the object
-              for (let key in parsedJson) {
-                if (parsedJson.hasOwnProperty(key)) {
-                  deepCheck(parsedJson[key], results);
-                  // Check if the key is "EvseStatus" and its value is not "Available" or "Occupied"
-                  if (key === 'EvseStatus' && parsedJson[key] !== 'Available' && parsedJson[key] !== 'Occupied') {
-                    // If the value is not "Available" or "Occupied", add an object with "EvseID" and "EvseStatus" to the results array
-                    results.push({
-                      EvseID: parsedJson.EvseID,
-                      EvseStatus: parsedJson.EvseStatus
-                    });
-                  }
-                }
-              }
-            }
-            
-            return results;
-          }
+      });
+    });
+
+    console.log(resultString);
+    console.log(deepCheck(parsedJson));
+
+    document.querySelector(".download-button").addEventListener("click", (event) => {
+      const blob = new Blob([resultString], { type: "text/csv" });
+
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+
+      link.download = "Wrong EvseValues Result.csv";
+      link.target = "_blank";
+
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+    });
+  });
+}
+
+function deepCheck(parsedJson, results) {
+  results = results || [];
+
+
+  if (parsedJson == null || typeof parsedJson !== "object") {
+    return results;
+  }
+
+
+  if (Array.isArray(parsedJson)) {
+    for (let i = 0; i < parsedJson.length; i++) {
+      deepCheck(parsedJson[i], results);
+    }
+  } else {
+    for (let key in parsedJson) {
+      if (parsedJson.hasOwnProperty(key)) {
+        deepCheck(parsedJson[key], results);
+        if (key === "EvseStatus" && parsedJson[key] !== "Available" && parsedJson[key] !== "Occupied") {
+          results.push({
+            EvseID: parsedJson.EvseID,
+            EvseStatus: parsedJson.EvseStatus,
+          });
+        }
+      }
+    }
+  }
+
+  return results;
+}
